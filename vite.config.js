673e9b9b167fc7eca1b6eceee5cc79ca7a40a493
Hudayfah <hlamine004@gmail.com>
2024-11-09 14:08:11 +0000

    More fixes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tic-tac-toe/', // Use only the repository name here
+  base: '/tic-tac-toe/'
 })
+
